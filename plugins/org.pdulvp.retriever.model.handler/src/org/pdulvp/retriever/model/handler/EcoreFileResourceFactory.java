b3e8b4ea5a24d65ca9f4b122400ad4273e18bc67
pdulvp <pdulvp@laposte.net>
2020-06-03 14:51:59 +0200

    Can't create Ecore models #3
    Signed-off-by: pdulvp <pdulvp@laposte.net>

--- plugins/org.pdulvp.retriever.model.handler/src/org/pdulvp/retriever/model/handler/EcoreFileResourceFactory.java
+++ plugins/org.pdulvp.retriever.model.handler/src/org/pdulvp/retriever/model/handler/EcoreFileResourceFactory.java
@@ -3,6 +3,8 @@ package org.pdulvp.retriever.model.handler;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 
@@ -18,16 +20,14 @@ public class EcoreFileResourceFactory extends EcoreResourceFactoryImpl {
   public Resource createResource(URI uri) {
     // If URI is already loaded in the EPackage.Registry,
     // we retrieve this resource instead of loading a new one
-    EPackage correspondingPackage = null;
-    String a = uri.lastSegment().substring(0, uri.fileExtension().length() - 1);
-    for (Object pkg : EPackage.Registry.INSTANCE.values()) {
-      if (pkg instanceof EPackage && ((EPackage) pkg).getNsPrefix().equals(a)) {
-        correspondingPackage = (EPackage) pkg;
-        break;
-      }
-    }
-    if (correspondingPackage != null) {
-      Resource existingResource = correspondingPackage.eResource();
+    // For Ecore.ecore, it will allow to create real EClass
+    
+    ResourceSet set = new ResourceSetImpl();
+    Resource first = set.getResource(uri, true);
+    String nsUri = ((EPackage)first.getContents().get(0)).getNsURI();
+    EPackage current = EPackage.Registry.INSTANCE.getEPackage(nsUri);
+    if (current != null) {
+      Resource existingResource = current.eResource();
       if (existingResource != null) {
         return existingResource;
       }
